0c871af0cd8821291db6be95cbb2b72770bea13f
Edevaldo-Cruz <edevaldo_cruz@hotmail.com>
2023-01-29 17:08:33 -0300

    Iniciando backend

---
+++ back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProAtividade.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AtividadeController : Controller
+    {
+        [HttpGet]
+       public string Get()
+        {
+            return "meu primeiro metodo get";
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "meu primeiro metodo get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "meu primeiro metodo Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int Id)
+        {
+            return "meu primeiro metodo Put";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "meu primeiro metodo Delete";
+        }
+    }
+}
